d76c2a04e99e2c80056a8d531654b54514d1d682
vitor-developer <vitordeveloper5@gmail.com>
2024-04-08 17:15:32 -0300

    tese

--- eryckgym/settings.py
+++ eryckgym/settings.py
@@ -17,7 +17,7 @@ DEBUG = config('DEBUG', cast=bool)
 
 
 ALLOWED_HOSTS = ['https://erickgym-6822.onrender.com','127.0.0.1','https://127.0.0.1:5500']
-CORS_ALLOWED_ORIGINS = config('CORS_ALLOWED_ORIGINS').split(',')
+CORS_ALLOWED_ORIGINS=['https://erickgym-6822.onrender.com','https://127.0.0.1:5500']
 
 #CSRF_TRUSTED_ORIGINS = []
 # Application definition
