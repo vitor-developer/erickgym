f3e3c67a7ef6d05570dd10e3e80a32dfc1e1c313
vitor-developer <vitordeveloper5@gmail.com>
2024-04-08 16:05:59 -0300

    renomear

--- treinos/models.py
+++ treinos/models.py
@@ -3,8 +3,8 @@ from django.db import models
 class Exercicio(models.Model):
     nome = models.CharField(max_length=250)
     descricao = models.CharField(max_length=500)
-    ativo = models.BooleanField(default=True)
+    esta_ativo = models.BooleanField(default=True)
     idade_minima_aluno = models.PositiveIntegerField(default=12)
 
     def __str__(self):
-            return f'Nome: {self.nome} | Descrição: {self.descricao} | Ativo: {self.ativo} | Idade Mínima: {self.idade_minima_aluno}'
+            return f'Nome: {self.nome} | Descrição: {self.descricao} | Ativo: {self.esta_ativo} | Idade Mínima: {self.idade_minima_aluno}'
